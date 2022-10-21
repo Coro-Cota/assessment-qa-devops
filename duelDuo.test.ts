5ef957b32c2860b4fa5b556042091a45168e8cd7
Nicolas Garaycochea <nicolas.garaycochea@gmail.com>
2022-10-21 13:16:23 -0700

    added some new tests

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -19,12 +19,24 @@ test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
+    await driver.sleep(2000)
 
     let drawCards = await driver.findElement(By.id('draw'))
     await drawCards.click()
+    expect(displayed).toBe(true)
     await driver.sleep(2000)
 
-    let addDuo = await driver.findElement(By.className('bot-btn'))
-    await addDuo.click()
+    let addDuo1 = await driver.findElement(By.className('bot-btn'))
+    await addDuo1.click()
+    expect(displayed).toBe(true)
+    await driver.sleep(2000)
+
+    let addDuo2 = await driver.findElement(By.className('bot-btn'))
+    await addDuo2.click()
+    expect(displayed).toBe(true)
+    await driver.sleep(2000)
+
+    let time2Duel = await driver.findElement(By.id('duel'))
+    await time2Duel.click()
     await driver.sleep(2000)
 })
