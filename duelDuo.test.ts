28e32ef401620789c0eaab4bcb47e958917f940c
Nicolas Garaycochea <nicolas.garaycochea@gmail.com>
2022-10-21 09:50:28 -0700

    finished automation section

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -3,7 +3,9 @@ import { Builder, Capabilities, By } from "selenium-webdriver"
 
 require('chromedriver')
 
-const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+const driver = new Builder()
+.withCapabilities(Capabilities.chrome())
+.build()
 
 beforeEach(async () => {
     driver.get('http://localhost:3000/')
@@ -17,4 +19,12 @@ test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
+
+    let drawCards = await driver.findElement(By.id('draw'))
+    await drawCards.click()
+    await driver.sleep(2000)
+
+    let addDuo = await driver.findElement(By.className('bot-btn'))
+    await addDuo.click()
+    await driver.sleep(2000)
 })
